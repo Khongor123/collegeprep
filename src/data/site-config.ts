03aae65a64722e0caf9a907967edb71fc2738215
Khongor123 <khongor750@gmail.com>
2024-02-11 16:04:24 +0800

    Initial Commit

---
+++ src/data/site-config.ts
@@ -0,0 +1,113 @@
+export type Image = {
+    src: string;
+    alt?: string;
+    caption?: string;
+};
+
+export type Link = {
+    text: string;
+    href: string;
+};
+
+export type Hero = {
+    title?: string;
+    text?: string;
+    image?: Image;
+    actions?: Link[];
+};
+
+export type Subscribe = {
+    title?: string;
+    text?: string;
+    formUrl: string;
+};
+
+export type SiteConfig = {
+    logo?: Image;
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    headerNavLinks?: Link[];
+    footerNavLinks?: Link[];
+    socialLinks?: Link[];
+    hero?: Hero;
+    subscribe?: Subscribe;
+    postsPerPage?: number;
+    projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    title: 'College Prep',
+    subtitle: 'Growing resources for you to prepare for college',
+    description: 'Collective resources for college prep',
+    image: {
+        src: '/dante-preview.jpg',
+        alt: 'Dante - Astro.js and Tailwind CSS theme'
+    },
+    headerNavLinks: [
+        {
+            text: 'Home',
+            href: '/'
+        },
+        {
+            text: 'Projects',
+            href: '/projects'
+        },
+        {
+            text: 'Blog',
+            href: '/blog'
+        },
+        {
+            text: 'Tags',
+            href: '/tags'
+        }
+    ],
+    footerNavLinks: [
+        // TODO
+        // {
+        //     text: 'About',
+        //     href: '/about'
+        // },
+        {
+            text: 'Contact',
+            href: '/contact'
+        },
+        {
+            text: 'Terms',
+            href: '/terms'
+        }
+    ],
+    socialLinks: [
+        {
+            text: 'Dribbble',
+            href: 'https://dribbble.com/'
+        },
+        {
+            text: 'Instagram',
+            href: 'https://instagram.com/'
+        },
+        {
+            text: 'X/Twitter',
+            href: 'https://twitter.com/'
+        }
+    ],
+    hero: {
+        title: 'Hi There *Traveller*',
+        text: "I'm **Khongor**, a high-school junior set to graduate in June 2025. As my college applications are near, I've decided to start preparing as early as possible and have obtained quite some knowledge and resources, hence deciding to share it online (also organizing everything).",
+        image: {
+            src: '/hero.jpeg',
+            alt: 'A person sitting at a desk in front of a computer'
+        },
+        actions: [
+            {
+                text: 'Get in Touch',
+                href: '/contact'
+            }
+        ]
+    },
+    postsPerPage: 8,
+    projectsPerPage: 8
+};
+
+export default siteConfig;
