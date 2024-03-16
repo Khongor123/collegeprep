0b117bcf79296e1a5ea1093635f906a951191cca
Khongor123 <khongor750@gmail.com>
2024-03-16 13:27:39 +0800

    Theme toggle size and Favicon change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://collegeprep-six.vercel.com',
-  integrations: [mdx(), sitemap(), tailwind({
-    applyBaseStyles: false
-  })],
-  output: "server",
-  adapter: vercel()
+    site: 'https://coprep.vercel.app',
+    integrations: [
+        mdx(),
+        sitemap(),
+        tailwind({
+            applyBaseStyles: false
+        })
+    ],
+    output: 'server',
+    adapter: vercel()
 });
