afe7e464d7427e6ebb38789c4def396ae754999a
Khongor123 <khongor750@gmail.com>
2025-06-30 11:56:52 +0800

    Repair work

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/intro-to-SAT-Math_BHa69jNn.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./intro-to-SAT-Math_DBDg6Vik.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
